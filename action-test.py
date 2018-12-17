40ed3f05cdb7a3689fbeb54c1423177207d61fe9
pierro <pierro@pcloud.fr>
2018-12-17 23:18:51 +0100

    Correciont intent name

--- action-test.py
+++ action-test.py
@@ -45,7 +45,7 @@ def intent_received(hermes, intent_message):
 	print(intent_message.intent.intent_name)
 	print ()
 
-	if intent_message.intent.intent_name == 'Joseph:askTime':
+	if intent_message.intent.intent_name == 'Joseph:askTest':
 
 		sentence = 'Il est '
 		print(intent_message.intent.intent_name)
